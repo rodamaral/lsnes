97c187acb986ef8434172dce47e308fd0bb66d19
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2011-10-28 20:39:20 +0300

    Add dummy enable-sound command to sound-dummy

--- dummy/sound-dummy.cpp
+++ dummy/sound-dummy.cpp
@@ -1,6 +1,22 @@
 #include "window.hpp"
 #include <cstdlib>
 #include <iostream>
+#include "command.hpp"
+
+namespace
+{
+	function_ptr_command<const std::string&> enable_sound("enable-sound", "Enable/Disable sound",
+		"Syntax: enable-sound <on/off>\nEnable or disable sound.\n",
+		[](const std::string& args) throw(std::bad_alloc, std::runtime_error) {
+			std::string s = args;
+			if(s == "on" || s == "true" || s == "1" || s == "enable" || s == "enabled")
+				;
+			else if(s == "off" || s == "false" || s == "0" || s == "disable" || s == "disabled")
+				;
+			else
+				throw std::runtime_error("Bad sound setting");
+		});
+}
 
 void sound_init() {}
 void sound_quit() {}
