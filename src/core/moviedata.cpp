c905c698f5a2c797455c815c180945b5256cbca2
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2012-02-06 21:59:56 +0200

    Also set core ports when loading savestate
    While patched bsnes versions automatically reassign port types on
    loadstate, future stock versions may not do so, so do it manually.

--- src/core/moviedata.cpp
+++ src/core/moviedata.cpp
@@ -349,8 +349,9 @@ void do_load_state(struct moviefile& _movie, int lmode)
 
 		if(will_load_state) {
 			//Load the savestate and movie state.
-			controls.set_port(0, _movie.port1, false);
-			controls.set_port(1, _movie.port2, false);
+			//Set the core ports in order to avoid port state being reinitialized when loading.
+			controls.set_port(0, _movie.port1, true);
+			controls.set_port(1, _movie.port2, true);
 			load_core_state(_movie.savestate);
 		} else {
 			load_sram(_movie.movie_sram);
