c5633cc2ea2c333a83debbdd85010b48f78c72fc
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2012-02-23 23:18:41 +0200

    Fix (i)strip_CR

--- src/library/string.cpp
+++ src/library/string.cpp
@@ -9,16 +9,15 @@ std::string strip_CR(const std::string& str)
 
 void istrip_CR(std::string& str)
 {
-	std::string x = str;
 	size_t crc = 0;
-	size_t xl = x.length();
+	size_t xl = str.length();
 	while(crc < xl) {
-		char y = x[xl - crc - 1];
+		char y = str[xl - crc - 1];
 		if(y != '\r' && y != '\n')
 			break;
 		crc++;
 	}
-	x.substr(0, xl - crc);
+	str = str.substr(0, xl - crc);
 }
 
 int firstchar(const std::string& str)
