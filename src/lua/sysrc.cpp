d6a17bde89f0b78eb29c3648f1d09c676c709100
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2012-09-17 22:36:10 +0300

    Lua: loopwarapper

---
+++ src/lua/sysrc.cpp
@@ -0,0 +1,18 @@
+const char* lua_sysrc_script =
+"loopwrapper = function(fn, ...)\n"
+"	local routine = coroutine.create(fn);\n"
+"	local resume = function(...)\n"
+"		if coroutine.status(routine) ~= \"dead\" then\n"
+"			local x, y;\n"
+"			x, y = coroutine.resume(routine, ...);\n"
+"			if not x then\n"
+"				error(y);\n"
+"			end\n"
+"		end\n"
+"	end\n"
+"	local yield = function()\n"
+"		return coroutine.yield(routine);\n"
+"	end\n"
+"	resume(yield, ...);\n"
+"	return resume;\n"
+"end\n";
