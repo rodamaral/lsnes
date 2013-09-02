67484f1ea514b768785e6ac5534b97048fea5346
Ilari Liusvaara <ilari.liusvaara@elisanet.fi>
2013-09-02 23:30:30 +0300

    enumerate_directory: Don't list . and ..

--- src/library/directory.cpp
+++ src/library/directory.cpp
@@ -13,7 +13,7 @@ std::set<std::string> enumerate_directory(const std::string& dir, const std::str
 		return x;
 	}
 	while((d2 = readdir(d)))
-		if(regex_match(match, d2->d_name))
+		if(regex_match(match, d2->d_name) && strcmp(d2->d_name, ".") && strcmp(d2->d_name, ".."))
 			x.insert(dir + "/" + d2->d_name);
 	closedir(d);
 	return x;
